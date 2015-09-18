a2a736d644d064c8981aa7f7ddc988b9ad2fa994
Radoslaw Karkut <rkarkut@implix.com>
2015-09-18 15:12:34 +0200

    fix

--- init.sh
+++ init.sh
@@ -9,9 +9,7 @@ echo "==> Clone protobox..."
 git clone git@github.com:protobox/protobox.git wp-vagrant
 echo "==> Copy config file..."
 
-git clone git@github.com:slummer87/vagrant-protobox-wordpress.git tmp_config
-
-cp tmp_config/common.yml wp-vagrant/data/config
+cp common.yml $directory/data/config
 
 cd $directory
 
@@ -23,10 +21,10 @@ vagrant up
 echo "==> Clone GetResponse plugin..."
 git clone git@git.int.getresponse.com:integrations/wp-getresponse-integration.git web/wp-getresponse-integration
 
-cp ../tmp_config/run.sh web
+cp ../run.sh web
 chmod 777 web/run.sh
 
 vagrant ssh -- -t 'sh /vagrant/web/run.sh'
 
-#echo "==> Add domain gr-wordpress.app to hosts"
-#echo "192.168.5.99    gr-wordpress.app" >> /etc/hosts
+echo "==> Add domain gr-wordpress.app to hosts"
+echo "192.168.5.99    gr-wordpress.app" >> /etc/hosts
